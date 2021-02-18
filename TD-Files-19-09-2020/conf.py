5536fe7608d2629ab704d3fcdb5cb2cad100f936
Shanmukh1501 <79193246+Shanmukh1501@users.noreply.github.com>
2021-02-18 16:50:20 +0530

    Update conf.py

--- TD-Files-19-09-2020/conf.py
+++ TD-Files-19-09-2020/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'E-commerce project using Java'
-copyright = '2020, Harideep'
-author = 'Harideep'
+project = 'GITHUB SC Management'
+copyright = '2020, Shanmukh'
+author = 'Shanmukh'
 
 # The full version, including alpha/beta/rc tags
 release = ''
